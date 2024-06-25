81028d733a81d304d3112501a995bce8f8d08dc3
Arthur Akhmetov <artur.akhmetov.2008@gmail.com>
2024-06-25 23:10:17 +0300

    add four cards for main page

--- src/services/server.js
+++ src/services/server.js
@@ -39,6 +39,38 @@ export const games = [
 		img: 'https://upload.wikimedia.org/wikipedia/en/5/51/Minecraft_cover.png',
 		price: 12,
 	},
+	{
+		id: 6,
+		title: 'Stellar Blade',
+		description:
+			'Игроки берут на себя командование главной героиней Евой, которая решается спасти человечество от беспощадной войны, которую ведут чудовищные существа в далеком будущем .',
+		img: 'https://image.api.playstation.com/vulcan/ap/rnd/202401/2212/281e4a2a7d4db5290f4dbe5c3fbb839315b808c95283e0f3.jpg',
+		price: 55,
+	},
+	{
+		id: 7,
+		title: 'The Last of Us Part II',
+		description:
+			'Спустя пять лет после страшного путешествия по охваченной эпидемией Америке Элли и Джоэл осели в Джексоне, штат Вайоминг. Обосновавшись в процветающей общине, они обрели желанную стабильность, несмотря на постоянную угрозу нападения зараженных и теряющих смысл жизни выживших.',
+		img: 'https://m.media-amazon.com/images/M/MV5BODUwNWY5YjctNDZkNy00ZTY1LWEzMzItZGVkYTllOWVjOTc3XkEyXkFqcGdeQXVyNjU4NTIxNzI@._V1_FMjpg_UX1000_.jpg',
+		price: 90,
+	},
+	{
+		id: 8,
+		title: 'SEKIRO SHADOWS DIE TWICE',
+		description:
+			'жестокий приключенческий боевик от третьего лица, действие которого происходит в кровавой Японии XIV века.',
+		img: 'https://www.sekirothegame.com/content/dam/atvi/sekiro/buy/box-art/4089_d20180531-001_Angled_FOB_Packshots_PC_EN_C_FINAL.png',
+		price: 45,
+	},
+	{
+		id: 9,
+		title: 'ELDEN RING',
+		description:
+			'Действие игры происходит в Междуземье — стране «за завесой тумана», которой с давних времён правила королева Марика Вечная. В Междуземье воля его правителя определяет саму реальность. ',
+		img: 'https://m.media-amazon.com/images/I/71lS5rBEFmL._SL1500_.jpg',
+		price: 31,
+	},
 ]
 
 export const friends = [
