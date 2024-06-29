a6460cf70d0d6bcc8277ada848976f7096daa8e5
Arthur Akhmetov <artur.akhmetov.2008@gmail.com>
2024-06-29 13:21:47 +0300

    try project to github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
+import { VitePluginGhPages } from 'vite-plugin-gh-pages'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), VitePWA(), VitePluginGhPages()],
 })
