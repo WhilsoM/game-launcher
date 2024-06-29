8e7228f0f2bafa8b0a51d828fe15fdd2ddeb6cad
Arthur Akhmetov <artur.akhmetov.2008@gmail.com>
2024-06-29 16:12:35 +0300

    try deploy to github pages #2

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
-import { VitePluginGhPages } from 'vite-plugin-gh-pages'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-	plugins: [react(), VitePWA(), VitePluginGhPages()],
+	plugins: [react()],
 })
